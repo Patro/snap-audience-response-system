4917b9da359a848b74b53c9a5a3c37df9481cca3
Patro <code@patrickrompf.de>
2019-02-20 19:22:58 +0100

    Add model user

---
+++ db/migrate/20190220181627_create_users.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.datetime :created_at, null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_185231) do
+ActiveRecord::Schema.define(version: 2019_02_20_181627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+  end
 
 end
