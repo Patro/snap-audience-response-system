728c8ba3844760ab52f7db75b0d6f5812ec65022
Patro <code@patrickrompf.de>
2019-03-04 10:52:10 +0100

    Add serializer for multiple choice question

---
+++ spec/serializers/multiple_choice_question_serializer_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MultipleChoiceQuestionSerializer do
+  let(:interactive_session) { create(:interactive_session) }
+  let(:question) do
+    create(
+      :multiple_choice_question,
+      interactive_session: interactive_session,
+      text: 'What is the answer of everything?'
+    )
+  end
+  let(:serializer) do
+    MultipleChoiceQuestionSerializer.new(
+      question,
+      params: { current_user: create(:user) }
+    )
+  end
+  let(:data) { serializer.serializable_hash[:data] }
+
+  describe 'data' do
+    subject { data }
+
+    it 'should serialize type' do
+      is_expected.to include(type: :multiple_choice_question)
+    end
+  end
+end
