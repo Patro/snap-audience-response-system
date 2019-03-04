82ee3d990036ae4bd6ee62ef2b6c022beb55c2ab
Patro <code@patrickrompf.de>
2019-03-04 10:58:26 +0100

    Add serializer for single choice question

---
+++ app/serializers/single_choice_question_serializer.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class SingleChoiceQuestionSerializer < QuestionSerializer
+end
---
+++ spec/serializers/single_choice_question_serializer_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SingleChoiceQuestionSerializer do
+  let(:interactive_session) { create(:interactive_session) }
+  let(:question) do
+    create(
+      :single_choice_question,
+      interactive_session: interactive_session,
+      text: 'What is the answer of everything?'
+    )
+  end
+  let(:serializer) do
+    SingleChoiceQuestionSerializer.new(
+      question,
+      params: { current_user: create(:user) }
+    )
+  end
+  let(:data) { serializer.serializable_hash[:data] }
+
+  describe 'data' do
+    subject { data }
+
+    it 'should serialize type' do
+      is_expected.to include(type: :single_choice_question)
+    end
+  end
+end
