3ca1c58d2e8d14651fb01bc9db3c267cc5a1d98d
Patro <code@patrickrompf.de>
2019-03-11 15:33:20 +0100

    Update question serializer to delegate to special serializers depending on STI type attribute

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -6,5 +6,20 @@ class QuestionSerializer < ApplicationSerializer
     policy_scope = Pundit.policy_scope!(params[:current_user], QuestionOption)
     record.options.merge(policy_scope)
   end
-  attributes :text, :type
+  attributes :text
+
+  class << self
+    def record_hash(record, fieldset, params = {})
+      return super if record.type.blank?
+
+      type_serializer = (record.type + 'Serializer').constantize
+      return super if ancestors.include?(type_serializer)
+
+      type_serializer.record_hash(record, fieldset, params)
+    rescue NameError
+      record_hash = super
+      record_hash[:type] = record.type.underscore.to_sym
+      record_hash
+    end
+  end
 end
--- spec/serializers/question_serializer_spec.rb
+++ spec/serializers/question_serializer_spec.rb
@@ -4,67 +4,129 @@ require 'rails_helper'
 
 RSpec.describe QuestionSerializer do
   let(:interactive_session) { create(:interactive_session) }
-  let(:question) do
-    create(:dummy_question, interactive_session: interactive_session,
-                            text: 'What is the answer of everything?')
-  end
   let(:serializer) do
-    QuestionSerializer.new(question, params: { current_user: create(:user) })
+    QuestionSerializer.new(resource, params: { current_user: create(:user) })
   end
   let(:data) { serializer.serializable_hash[:data] }
 
-  describe 'data' do
-    subject { data }
+  context 'given dummy question' do
+    let(:question) do
+      create(:dummy_question, interactive_session: interactive_session,
+                              text: 'What is the answer of everything?')
+    end
+    let(:resource) { question }
 
-    it { is_expected.to include(id: "#{question.id}") }
-    it { is_expected.to include(type: :question) }
+    describe 'data' do
+      subject { data }
 
-    describe '> attributes' do
-      subject { data[:attributes] }
+      it { is_expected.to include(id: "#{question.id}") }
+      it { is_expected.to include(type: :dummy_question) }
 
-      it { is_expected.to include(text: 'What is the answer of everything?') }
+      describe '> attributes' do
+        subject { data[:attributes] }
+
+        it { is_expected.to include(text: 'What is the answer of everything?') }
+      end
+
+      describe '> relationships' do
+        describe '> interactive session' do
+          subject { data[:relationships][:interactive_session][:data] }
+
+          it { is_expected.to include_identifier_of(interactive_session) }
+        end
+
+        describe '> options' do
+          subject { data[:relationships][:options][:data] }
+
+          context 'given question without options' do
+            it { is_expected.to be_empty }
+          end
+
+          context 'given question with option' do
+            let!(:question_option) do
+              create(:question_option, question: question)
+            end
+
+            context 'given policy scope that permits access' do
+              before(:each) do
+                allow_any_instance_of(QuestionOptionPolicy::Scope)
+                .to receive(:resolve)
+                .and_return(QuestionOption.all)
+              end
+
+              it { is_expected.to include_identifier_of(question_option) }
+            end
+
+            context 'given policy scope that denies access' do
+              before(:each) do
+                allow_any_instance_of(QuestionOptionPolicy::Scope)
+                .to receive(:resolve)
+                .and_return(QuestionOption.none)
+              end
+
+              it { is_expected.not_to include_identifier_of(question_option) }
+            end
+          end
+        end
+      end
+    end
+  end
+
+  context 'given multiple choice question' do
+    let(:question) do
+      create(:multiple_choice_question)
+    end
+    let(:resource) { question }
+
+    it 'should delegate serialization to special serializer' do
+      expect(MultipleChoiceQuestionSerializer)
+      .to receive(:record_hash)
+
+      serializer.serializable_hash
     end
 
-    describe '> relationships' do
-      describe '> interactive session' do
-        subject { data[:relationships][:interactive_session][:data] }
+    describe 'data' do
+      subject { data }
 
-        it { is_expected.to include_identifier_of(interactive_session) }
-      end
+      it { is_expected.to include_identifier_of(question) }
+    end
+  end
 
-      describe '> options' do
-        subject { data[:relationships][:options][:data] }
+  context 'given single choice question' do
+    let(:question) do
+      create(:single_choice_question)
+    end
+    let(:resource) { question }
 
-        context 'given question without options' do
-          it { is_expected.to be_empty }
-        end
+    it 'should delegate serialization to special serializer' do
+      expect(SingleChoiceQuestionSerializer)
+      .to receive(:record_hash)
 
-        context 'given question with option' do
-          let!(:question_option) do
-            create(:question_option, question: question)
-          end
+      serializer.serializable_hash
+    end
 
-          context 'given policy scope that permits access' do
-            before(:each) do
-              allow_any_instance_of(QuestionOptionPolicy::Scope)
-              .to receive(:resolve)
-              .and_return(QuestionOption.all)
-            end
+    describe 'data' do
+      subject { data }
 
-            it { is_expected.to include_identifier_of(question_option) }
-          end
+      it { is_expected.to include_identifier_of(question) }
+    end
+  end
 
-          context 'given policy scope that denies access' do
-            before(:each) do
-              allow_any_instance_of(QuestionOptionPolicy::Scope)
-              .to receive(:resolve)
-              .and_return(QuestionOption.none)
-            end
+  context 'given collection of questions' do
+    let(:dummy_question) { create(:dummy_question) }
+    let(:multiple_choice_question) { create(:multiple_choice_question) }
+    let(:single_choice_question) { create(:single_choice_question) }
+    let(:questions) do
+      [dummy_question, multiple_choice_question, single_choice_question]
+    end
+    let(:resource) { questions }
 
-            it { is_expected.not_to include_identifier_of(question_option) }
-          end
-        end
-      end
+    describe 'data' do
+      subject { data }
+
+      it { is_expected.to include_identifier_of(dummy_question) }
+      it { is_expected.to include_identifier_of(multiple_choice_question) }
+      it { is_expected.to include_identifier_of(single_choice_question) }
     end
   end
 end
