37ae8ccb29c2d0aaccb5ca86ce2a2025e6df03fc
Patro <code@patrickrompf.de>
2019-03-11 12:30:49 +0100

    Extend attendance policy

--- app/policies/attendance_policy.rb
+++ app/policies/attendance_policy.rb
@@ -1,7 +1,45 @@
 # frozen_string_literal: true
 
 class AttendancePolicy < ApplicationPolicy
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      own_attendances.or(attendances_of_owned_sessions)
+    end
+
+    private
+
+      def own_attendances
+        scope.where(attendee: user)
+      end
+
+      def attendances_of_owned_sessions
+        scope.where(interactive_session: user.owned_interactive_sessions)
+      end
+  end
+
+  def index?
+    true
+  end
+
   def create?
     true
   end
+
+  def show?
+    owner_of_session? || attendee?
+  end
+
+  def destroy?
+    owner_of_session? || attendee?
+  end
+
+  private
+
+    def attendee?
+      user.eql?(record.attendee)
+    end
+
+    def owner_of_session?
+      user.eql?(record.interactive_session.owner)
+    end
 end
--- spec/policies/attendance_policy_spec.rb
+++ spec/policies/attendance_policy_spec.rb
@@ -4,12 +4,79 @@ require 'rails_helper'
 
 RSpec.describe AttendancePolicy do
   let(:attendance) { create(:attendance) }
+  let(:owner) { attendance.interactive_session.owner }
+  let(:attendee) { attendance.attendee }
   subject { AttendancePolicy.new(user, attendance) }
+
+  describe 'Scope' do
+    let(:scope) { AttendancePolicy::Scope.new(user, Attendance) }
+    subject { scope.resolve }
+
+    context 'as owner' do
+      let(:user) { owner }
+
+      it 'should include attendance of owned interactive session' do
+        is_expected.to include(attendance)
+      end
+
+      context 'given attendance of unassociated interactive session' do
+        let!(:other) { create(:attendance) }
+
+        it 'should not include attendance' do
+          is_expected.not_to include(other)
+        end
+      end
+    end
+
+    context 'as attendee' do
+      let(:user) { attendee }
+
+      it 'should include own attendance' do
+        is_expected.to include(attendance)
+      end
+
+      context 'given unassociated attendance' do
+        let!(:other) { create(:attendance) }
+
+        it 'should not include attendance' do
+          is_expected.not_to include(other)
+        end
+      end
+    end
+
+    context 'as virgin user' do
+      let(:user) { create(:user) }
+
+      it 'should not include attendance' do
+        is_expected.not_to include(attendance)
+      end
+    end
+  end
+
+  context 'as owner' do
+    let(:user) { owner }
+
+    it { is_expected.to permit(:index) }
+    it { is_expected.to permit(:create) }
+    it { is_expected.to permit(:show) }
+    it { is_expected.not_to permit(:update) }
+    it { is_expected.to permit(:destroy) }
+  end
+
+  context 'as attendee' do
+    let(:user) { attendee }
+
+    it { is_expected.to permit(:index) }
+    it { is_expected.to permit(:create) }
+    it { is_expected.to permit(:show) }
+    it { is_expected.not_to permit(:update) }
+    it { is_expected.to permit(:destroy) }
+  end
 
   context 'as virgin user' do
     let(:user) { create(:user) }
 
-    it { is_expected.not_to permit(:index) }
+    it { is_expected.to permit(:index) }
     it { is_expected.to permit(:create) }
     it { is_expected.not_to permit(:show) }
     it { is_expected.not_to permit(:update) }
