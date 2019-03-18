74a644bc9f39593423bc615d5795a4e213c724fd
Patro <code@patrickrompf.de>
2019-03-18 17:33:08 +0100

    Add on submit handler to join form

--- frontend/src/components/JoinSessionForm.js
+++ frontend/src/components/JoinSessionForm.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import { Form, Button, Input } from 'antd';
 
 class JoinSessionForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.form.validateFields((err, fieldsValue) => {
+      if (err) { return; }
+
+      const attendanceCode = fieldsValue['attendance_code'];
+      this.props.onSubmit(attendanceCode);
+    })
+  }
+
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
       <div className="JoinSessionForm">
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Item label="Attendance Code">
             { getFieldDecorator('attendance_code')(
               <Input placeholder="ABCD" />
--- frontend/src/components/JoinSessionForm.test.js
+++ frontend/src/components/JoinSessionForm.test.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { Button, Input } from 'antd';
+import { Form, Button, Input } from 'antd';
 import { mount } from 'enzyme';
 import JoinSessionForm from './JoinSessionForm';
 
 describe('JoinSessionForm', () => {
+  const getForm = (wrapper) => (
+    wrapper.find(Form)
+  );
+  const getAttendanceCodeInput = (wrapper) => (
+    wrapper.find(Input).filter('#attendance_code')
+  );
+  const getSubmitButton = (wrapper) => (
+    wrapper.find(Button).filter('[htmlType="submit"]')
+  );
+
   it('should render input box for attendance code', () => {
     const wrapper = mount(<JoinSessionForm />);
-    const input = wrapper.find(Input).filter('#attendance_code');
+    const input = getAttendanceCodeInput(wrapper);
     expect(input).toHaveLength(1);
   });
 
   it('should render submit button', () => {
     const wrapper = mount(<JoinSessionForm />);
-    const button = wrapper.find(Button);
+    const button = getSubmitButton(wrapper);
     expect(button).toHaveLength(1);
+  });
+
+  it('should call on submit handler with attendance code on form submit', () => {
+    const onSubmit = jest.fn();
+
+    const wrapper = mount(<JoinSessionForm onSubmit={onSubmit} />);
+    const input = getAttendanceCodeInput(wrapper);
+    input.simulate('change', { target: { value: 'ABCD' } });
+    const form = getForm(wrapper);
+    form.simulate('submit');
+
+    expect(onSubmit).toBeCalledWith('ABCD');
   });
 });
