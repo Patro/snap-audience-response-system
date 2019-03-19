a9c0b21b9ae0e796a95645c2a3a8b8feeef4b729
Patro <code@patrickrompf.de>
2019-03-19 13:42:02 +0100

    Add label to interactive session component

--- frontend/src/components/InteractiveSession.js
+++ frontend/src/components/InteractiveSession.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
 class InteractiveSession extends Component {
+  get attributes() {
+    if (this.props.interactiveSession === undefined) {
+      return {};
+    }
+    return this.props.interactiveSession.attributes;
+  }
+
   render() {
     return (
       <div className="InteractiveSession">
+        <h1 className="interactive_session__label">{this.attributes.label}</h1>
       </div>
     );
   }
--- frontend/src/components/InteractiveSession.test.js
+++ frontend/src/components/InteractiveSession.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import InteractiveSession from './InteractiveSession';
 
 describe('InteractiveSession', () => {
   it('renders without crashing', () => {
     mount(<InteractiveSession />);
+  });
+
+  it('renders label of interactive session', () => {
+    const interactiveSession = { attributes: { label: 'My Event'} };
+    const wrapper = shallow(
+      <InteractiveSession interactiveSession={interactiveSession} />
+    );
+    const label = wrapper.find('.interactive_session__label');
+    expect(label.text()).toEqual('My Event');
   });
 });
