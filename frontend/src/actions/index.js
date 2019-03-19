e95deaefdd57483e664a72dbf96165007d9824d0
Patro <code@patrickrompf.de>
2019-03-19 16:51:19 +0100

    Add action to create entities

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,6 +1,13 @@
 export const JOIN_SESSION = 'JOIN_SESSION';
+export const FETCH_ENTITY = 'FETCH_ENTITY';
 
 export const joinSession = (attendanceCode) => ({
   type: JOIN_SESSION,
   attendanceCode
+});
+
+export const fetchEntity = (entityType, entityId) => ({
+  type: FETCH_ENTITY,
+  entityType,
+  entityId
 });
