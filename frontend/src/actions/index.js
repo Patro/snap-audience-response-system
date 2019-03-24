efd483ed6da6b22bd7335602af34b8b3b9f9920b
Patro <code@patrickrompf.de>
2019-03-24 22:58:32 +0100

    Add action to save collection in store

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -2,6 +2,7 @@ export const JOIN_SESSION = 'JOIN_SESSION';
 export const FETCH_ENTITY = 'FETCH_ENTITY';
 export const RECEIVE_ENTITY = 'RECEIVE_ENTITY';
 export const FETCH_COLLECTION = 'FETCH_COLLECTION';
+export const RECEIVE_COLLECTION = 'RECEIVE_COLLECTION';
 
 export const joinSession = (attendanceCode) => ({
   type: JOIN_SESSION,
@@ -23,4 +24,11 @@ export const fetchCollection = (entityType, filterParams) => ({
   type: FETCH_COLLECTION,
   entityType,
   filterParams
+});
+
+export const receiveCollection = (entityType, filterParams, collection) => ({
+  type: RECEIVE_COLLECTION,
+  entityType,
+  filterParams,
+  collection
 });
