18ea2f97e95951b1210f27c4b040a11539288c13
Patro <code@patrickrompf.de>
2019-03-24 19:01:05 +0100

    Add action to fetch collections

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,6 +1,7 @@
 export const JOIN_SESSION = 'JOIN_SESSION';
 export const FETCH_ENTITY = 'FETCH_ENTITY';
 export const RECEIVE_ENTITY = 'RECEIVE_ENTITY';
+export const FETCH_COLLECTION = 'FETCH_COLLECTION';
 
 export const joinSession = (attendanceCode) => ({
   type: JOIN_SESSION,
@@ -16,4 +17,10 @@ export const fetchEntity = (entityType, entityId) => ({
 export const receiveEntity = (entity) => ({
   type: RECEIVE_ENTITY,
   entity
+});
+
+export const fetchCollection = (entityType, filterParams) => ({
+  type: FETCH_COLLECTION,
+  entityType,
+  filterParams
 });
