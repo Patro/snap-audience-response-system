43c102797be9092ff92b2ad6ccb29bffaa27d5c0
Patro <code@patrickrompf.de>
2019-03-24 22:35:53 +0100

    Implement selector to get collection from store

--- frontend/src/selectors/getCollection.js
+++ frontend/src/selectors/getCollection.js
@@ -1,3 +1,11 @@
-const getCollection = (state, type, filterParams) => {};
+import get from 'lodash/get';
+
+const getCollection = (state, type, filterParams = {}) => {
+  const collectionsOfType = get(state, `collections.${type}`);
+  if (collectionsOfType === undefined) { return; }
+
+  const filterKey = JSON.stringify(filterParams);
+  return collectionsOfType[filterKey];
+};
 
 export default getCollection;
---
+++ frontend/src/selectors/getCollection.test.js
@@ -0,0 +1,65 @@
+import getCollection from './getCollection';
+
+describe('getCollection', () => {
+  describe('given state with collections', () => {
+    const collection = {
+      entities: [
+        { type: 'SPACESHIP_ENGINE', id: 100 }
+      ]
+    };
+    const state = {
+      collections: {
+        SPACESHIP_ENGINE: {
+          '{"fuel":"gas"}': collection,
+        },
+      },
+    };
+
+    describe('when collection with filter params exists', () => {
+      it('returns collection', () => {
+        const filterParams = { fuel: 'gas' }
+        const result = getCollection(state, 'SPACESHIP_ENGINE', filterParams)
+        expect(result).toEqual(collection);
+      });
+    });
+
+    describe('when collection with filter params does not exist', () => {
+      it('returns undefined', () => {
+        const filterParams = { fuel: 'electricity' }
+        const result = getCollection(state, 'SPACESHIP_ENGINE', filterParams)
+        expect(result).toBeUndefined();
+      });
+    });
+  });
+
+  describe('given state with collections in differnt group', () => {
+    const collection = {
+      entities: [
+        { type: 'CAR', id: 100 }
+      ]
+    };
+    const state = {
+      collections: {
+        CAR: {
+          '{"fuel":"gas"}': collection,
+        },
+      },
+    };
+
+    it('returns undefined', () => {
+      const filterParams = { fuel: 'gas' }
+      const result = getCollection(state, 'SPACESHIP_ENGINE', filterParams)
+      expect(result).toBeUndefined();
+    });
+  });
+
+  describe('given empty state', () => {
+    const state = {};
+
+    it('returns undefined', () => {
+      const filterParams = { fuel: 'gas' }
+      const result = getCollection(state, 'SPACESHIP_ENGINE', filterParams)
+      expect(result).toBeUndefined()
+    });
+  });
+});
