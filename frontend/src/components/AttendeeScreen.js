9870d7f7d0570dcce769df1118c20f149e3ab078
Patro <code@patrickrompf.de>
2019-03-26 19:04:48 +0100

    Add waiting and success info to attendee screen

--- frontend/src/components/AttendeeScreen.js
+++ frontend/src/components/AttendeeScreen.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import { Alert, Icon } from 'antd';
 import RespondFormContainer from '../containers/RespondFormContainer';
 
 class AttendeeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { respondedToPoll: false };
+    this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this);
+  }
+
   componentDidMount() {
     this.refresh();
   }
@@ -12,19 +19,63 @@ class AttendeeScreen extends Component {
     }
   }
 
+  hasUnrespondedPoll() {
+    return this.props.unrespondedPoll !== undefined;
+  }
+
+  handleSuccessfulResponse() {
+    this.setState({ respondedToPoll: true });
+    this.refresh();
+  }
+
   render() {
     return (
       <div className="attendee_screen">
-        {this.renderRespondFormContainer()}
+        {this.renderSuccessInfo()}
+        {this.renderContent()}
       </div>
     );
   }
 
+  renderSuccessInfo() {
+    if (!this.state.respondedToPoll) { return; }
+    return (
+      <Alert
+        message="Response send"
+        description="Thank you for your response."
+        type="success"
+        showIcon
+        closable
+      />
+    )
+  }
+
+  renderContent() {
+    if (this.hasUnrespondedPoll()) {
+      return this.renderRespondFormContainer();
+    }
+    return this.renderWaitingInfo();
+  }
+
   renderRespondFormContainer() {
     const poll = this.props.unrespondedPoll;
-    if (poll === undefined) { return; }
+    return (
+      <RespondFormContainer
+        poll={poll}
+        onSuccess={this.handleSuccessfulResponse}/>
+    )
+  }
 
-    return (<RespondFormContainer poll={poll} />)
+  renderWaitingInfo() {
+    return (
+      <Alert
+        message="Waiting for next poll..."
+        description="There is no active poll at the moment. The next poll will be shown to you here automatically."
+        type="info"
+        icon={<Icon type="loading-3-quarters" spin />}
+        showIcon
+      />
+    )
   }
 }
 
--- frontend/src/components/AttendeeScreen.test.js
+++ frontend/src/components/AttendeeScreen.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import { Alert } from 'antd';
 import factories from '../../__factories__';
 import AttendeeScreen from './AttendeeScreen';
 import RespondFormContainer from '../containers/RespondFormContainer';
@@ -9,11 +10,48 @@ describe('AttendeeScreen', () => {
     mount(<AttendeeScreen />);
   });
 
-  it('renders respond form container', () => {
+  describe('without unresponded poll', () => {
+    it('renders waiting info', () => {
+      const wrapper = shallow(<AttendeeScreen  />);
+      const alert = wrapper.find(Alert).render();
+      expect(alert.text()).toContain('Waiting');
+    });
+  });
+
+  describe('given unresponded poll', () => {
     const poll = factories.poll.entity();
-    const wrapper = shallow(<AttendeeScreen unrespondedPoll={poll} />);
-    const form = wrapper.find(RespondFormContainer);
-    expect(form.length).toBe(1);
+
+    it('renders respond form container', () => {
+      const wrapper = shallow(<AttendeeScreen unrespondedPoll={poll} />);
+      const form = wrapper.find(RespondFormContainer);
+      expect(form.length).toBe(1);
+    });
+
+    describe('when user responded to poll', () => {
+      const simulateResponse = (wrapper) => {
+        const form = wrapper.find(RespondFormContainer);
+        form.simulate('success');
+      };
+
+      it('shows thank you message', () => {
+        const wrapper = shallow(<AttendeeScreen unrespondedPoll={poll} />);
+        simulateResponse(wrapper);
+
+        const alert = wrapper.find(Alert).render();
+        expect(alert.text()).toContain('Thank you');
+      });
+
+      it('calls on refresh handler', () => {
+        const refreshHandler = jest.fn();
+
+        const wrapper = shallow(
+          <AttendeeScreen unrespondedPoll={poll} onRefresh={refreshHandler} />
+        );
+        simulateResponse(wrapper);
+
+        expect(refreshHandler).toBeCalled();
+      });
+    });
   });
 
   it('calls on refresh handler on mount', () => {
