ac204a9a4f0e9486a4e212314c673e37d049470a
Patro <code@patrickrompf.de>
2019-03-26 11:53:34 +0100

    Refactor reducer tests

--- frontend/src/reducers/collections.test.js
+++ frontend/src/reducers/collections.test.js
@@ -2,34 +2,36 @@ import collections from './collections';
 import { receiveCollection } from '../actions';
 
 describe('collections reducer', () => {
-  it('should handle RECEIVE_COLLECTION', () => {
-    const stateBefore = {
-      CAR: {
-        '{}': { entities: [] },
-      },
-    };
-    const action = receiveCollection('VEHICLE', { fuel: 'gas' }, {
-      entities: [
-        { id: 2, type: 'CAR', attributes: { maxSpeed: 200 } },
-        { id: 2, type: 'SPACESHIP', attributes: { maxSpeed: 100000 } },
-        { id: 3, type: 'SPACESHIP', attributes: { maxSpeed: 400000 } },
-      ]
-    });
-    const stateAfter = {
-      CAR: {
-        '{}': { entities: [] },
-      },
-      VEHICLE: {
-        '{"fuel":"gas"}': {
-          entities: [
-            { id: 2, type: 'CAR' },
-            { id: 2, type: 'SPACESHIP' },
-            { id: 3, type: 'SPACESHIP' },
-          ]
-        }
-      },
-    };
+  describe('on RECEIVE_COLLECTION', () => {
+    it('adds collection to store', () => {
+      const stateBefore = {
+        CAR: {
+          '{}': { entities: [] },
+        },
+      };
+      const action = receiveCollection('VEHICLE', { fuel: 'gas' }, {
+        entities: [
+          { id: 2, type: 'CAR', attributes: { maxSpeed: 200 } },
+          { id: 2, type: 'SPACESHIP', attributes: { maxSpeed: 100000 } },
+          { id: 3, type: 'SPACESHIP', attributes: { maxSpeed: 400000 } },
+        ]
+      });
+      const stateAfter = {
+        CAR: {
+          '{}': { entities: [] },
+        },
+        VEHICLE: {
+          '{"fuel":"gas"}': {
+            entities: [
+              { id: 2, type: 'CAR' },
+              { id: 2, type: 'SPACESHIP' },
+              { id: 3, type: 'SPACESHIP' },
+            ]
+          }
+        },
+      };
 
-    expect(collections(stateBefore, action)).toEqual(stateAfter);
+      expect(collections(stateBefore, action)).toEqual(stateAfter);
+    });
   });
 });
--- frontend/src/reducers/entities.test.js
+++ frontend/src/reducers/entities.test.js
@@ -2,49 +2,53 @@ import entities from './entities';
 import { receiveEntity, receiveCollection } from '../actions';
 
 describe('entities reducer', () => {
-  it('should handle RECEIVE_COLLECTION', () => {
-    const stateBefore = {
-      CAR: {
-        1: { id: 1, type: 'CAR' },
-      },
-    };
-    const action = receiveCollection('VEHICLE', {}, {
-      entities: [
-        { id: 2, type: 'CAR', attributes: { maxSpeed: 200 } },
-        { id: 2, type: 'SPACESHIP', attributes: { maxSpeed: 100000 } },
-        { id: 3, type: 'SPACESHIP', attributes: { maxSpeed: 400000 } },
-      ]
-    });
-    const stateAfter = {
-      CAR: {
-        1: { id: 1, type: 'CAR' },
-        2: { id: 2, type: 'CAR', attributes: { maxSpeed: 200 } },
-      },
-      SPACESHIP: {
-        2: { id: 2, type: 'SPACESHIP', attributes: { maxSpeed: 100000 } },
-        3: { id: 3, type: 'SPACESHIP', attributes: { maxSpeed: 400000 } },
-      },
-    };
+  describe('on RECEIVE_COLLECTION', () => {
+    it('adds entities of collection to store', () => {
+      const stateBefore = {
+        CAR: {
+          1: { id: 1, type: 'CAR' },
+        },
+      };
+      const action = receiveCollection('VEHICLE', {}, {
+        entities: [
+          { id: 2, type: 'CAR', attributes: { maxSpeed: 200 } },
+          { id: 2, type: 'SPACESHIP', attributes: { maxSpeed: 100000 } },
+          { id: 3, type: 'SPACESHIP', attributes: { maxSpeed: 400000 } },
+        ]
+      });
+      const stateAfter = {
+        CAR: {
+          1: { id: 1, type: 'CAR' },
+          2: { id: 2, type: 'CAR', attributes: { maxSpeed: 200 } },
+        },
+        SPACESHIP: {
+          2: { id: 2, type: 'SPACESHIP', attributes: { maxSpeed: 100000 } },
+          3: { id: 3, type: 'SPACESHIP', attributes: { maxSpeed: 400000 } },
+        },
+      };
 
-    expect(entities(stateBefore, action)).toEqual(stateAfter);
+      expect(entities(stateBefore, action)).toEqual(stateAfter);
+    });
   });
 
-  it('should handle RECEIVE_ENTITY', () => {
-    const stateBefore = {
-      CAR: { 1: { id: 1, type: 'CAR' } },
-    };
-    const action = receiveEntity({
-      id: 2, type: 'SPACESHIP', attributes: { maxSpeed: 100000 },
-    });
-    const stateAfter = {
-      CAR: {
-        1: { id: 1, type: 'CAR' },
-      },
-      SPACESHIP: {
-        2: { id: 2, type: 'SPACESHIP', attributes: { maxSpeed: 100000 } },
-      },
-    };
+  describe('on RECEIVE_ENTITY', () => {
+    it('adds entity to store', () => {
+      const stateBefore = {
+        CAR: { 1: { id: 1, type: 'CAR' } },
+      };
+      const action = receiveEntity({
+        id: 2, type: 'SPACESHIP', attributes: { maxSpeed: 100000 },
+      });
+      const stateAfter = {
+        CAR: {
+          1: { id: 1, type: 'CAR' },
+        },
+        SPACESHIP: {
+          2: { id: 2, type: 'SPACESHIP', attributes: { maxSpeed: 100000 } },
+        },
+      };
 
-    expect(entities(stateBefore, action)).toEqual(stateAfter);
+      expect(entities(stateBefore, action)).toEqual(stateAfter);
+    });
   });
 });
