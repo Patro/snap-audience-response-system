35463835673706de51b79f80176bdb11c9639431
Patro <code@patrickrompf.de>
2019-03-26 19:12:22 +0100

    Add feature test for waiting message

--- spec/features/attendee_navigates_to_interactive_sessions_page_spec.rb
+++ spec/features/attendee_navigates_to_interactive_sessions_page_spec.rb
@@ -5,6 +5,18 @@ require 'rails_helper'
 RSpec.feature 'Attendee navigates to interactive sessions page', type: :feature do
   set_current_user
 
+  context 'without open poll' do
+    scenario 'they see a waiting message' do
+      interactive_session = create(:interactive_session)
+      create(:attendance, interactive_session: interactive_session,
+                          attendee: current_user)
+
+      visit "/interactive_sessions/#{interactive_session.id}"
+
+      expect(page).to have_text 'Waiting'
+    end
+  end
+
   context 'given open poll' do
     scenario 'they see the questions text' do
       interactive_session = create(:interactive_session)
