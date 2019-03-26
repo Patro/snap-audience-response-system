86ea2dd4a4825906f4ee7569239b93923bc0eccc
Patro <code@patrickrompf.de>
2019-03-26 16:16:17 +0100

    Add factories for succeeded and failed job

--- frontend/__factories__/job.js
+++ frontend/__factories__/job.js
@@ -1,9 +1,21 @@
 import defaultsDeep from 'lodash/defaultsDeep';
-import { STARTED } from './../src/constants/jobStatus';
+import interactiveSession from './interactiveSession';
+import { STARTED, SUCCEEDED, FAILED } from './../src/constants/jobStatus';
 
 export const started = (obj) => (defaultsDeep({}, obj, {
   id: 'jobId',
   status: STARTED,
 }));
 
-export default { started };
+export const succeeded = (obj) => (defaultsDeep({}, obj, {
+  id: 'jobId',
+  status: SUCCEEDED,
+  result: interactiveSession.entity(),
+}));
+
+export const failed = (obj) => (defaultsDeep({}, obj, {
+  id: 'jobId',
+  status: FAILED,
+}));
+
+export default { started, succeeded, failed };
