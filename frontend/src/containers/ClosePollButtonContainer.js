4630aac459b6c915cd5c1c37c23cd00103b41c2e
Patro <code@patrickrompf.de>
2019-04-29 12:29:51 +0200

    Add container for close poll button

---
+++ frontend/src/containers/ClosePollButtonContainer.js
@@ -0,0 +1,33 @@
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { updateEntity } from '../actions';
+import { getJob } from '../selectors';
+import withJob from './withJob';
+import ClosePollButton from '../components/ClosePollButton';
+
+const mapStateToProps = (state, { poll }) => ({
+  closePollJob: getJob(state, jobId(poll)),
+});
+
+const mapDispatchToProps = (dispatch, { poll }) => ({
+  onClose: () => setStatusToClosed(dispatch, poll),
+});
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withJob(props => props.closePollJob),
+)(ClosePollButton);
+
+///////////////////////////////////////////////////////////////////////////////
+
+const jobId = (poll) => (
+  `closePollJob:${poll.type}:${poll.id}`
+);
+
+const setStatusToClosed = (dispatch, poll) => {
+  const updatedPoll = {
+    ...poll,
+    attributes: { status: 'closed' },
+  };
+  dispatch(updateEntity(updatedPoll, jobId(poll)))
+};
---
+++ frontend/src/containers/ClosePollButtonContainer.test.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import factories from '../../__factories__';
+import AbstractTestWrapper from '../utils/AbstractTestWrapper';
+import { updateEntity } from '../actions';
+import ClosePollButton from '../components/ClosePollButton';
+import ClosePollButtonContainer from './ClosePollButtonContainer';
+
+class TestWrapper extends AbstractTestWrapper {
+  get closePollButton() {
+    return this.wrapper.find(ClosePollButton);
+  }
+
+  get givenClosePollJob() {
+    return this.closePollButton.prop('closePollJob');
+  }
+
+  _render() {
+    return mount(this._addStoreProvider(
+      <ClosePollButtonContainer {...this.props} />
+    ))
+  }
+
+  closePoll() {
+    this.closePollButton.props().onClose();
+  }
+}
+
+describe('ClosePollButtonContainer', () => {
+  let component, poll;
+
+  beforeEach(() => {
+    poll = factories.poll.entity({ id: 791 });
+    component = new TestWrapper({
+      props: { poll },
+    });
+  });
+
+  describe('close poll job', () => {
+    describe('given filled store', () => {
+      let job;
+
+      beforeEach(() => {
+        const jobId = 'closePollJob:POLL:791';
+        job = factories.job.started({ id: jobId });
+        component.store = {
+          jobs: {
+            [jobId]: job,
+          },
+        };
+      });
+
+      it('passes job to component', () => {
+        expect(component.givenClosePollJob).toEqual(job);
+      });
+    });
+
+    describe('given empty store', () => {
+      beforeEach(() => {
+        component.store = {};
+      });
+
+      it('passes undefined as job to component', () => {
+        expect(component.givenClosePollJob).toBeUndefined();
+      });
+    });
+  });
+
+  describe('on close', () => {
+    it('dispatches update entity action', () => {
+      component.closePoll();
+
+      const actions = component.store.getActions();
+      const expectedPoll = {
+        ...poll,
+        attributes: { status: 'closed' },
+      };
+      const expectedAction = updateEntity(expectedPoll, expect.anything());
+      expect(actions).toContainEqual(expectedAction);
+    });
+  });
+});
