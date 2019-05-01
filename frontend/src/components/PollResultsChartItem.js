00b4209d35013888cff7c8a2fd6cb27dd47e3ee7
Patro <code@patrickrompf.de>
2019-05-01 21:19:23 +0200

    Implement poll results chart item

--- frontend/src/components/PollResultsChartItem.js
+++ frontend/src/components/PollResultsChartItem.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react';
+import { Progress } from 'antd';
 
 class PollResultsChartItem extends Component {
+  get poll() {
+    return this.props.poll;
+  }
+
+  get questionOption() {
+    return this.props.questionOption;
+  }
+
+  get questionOptionCount() {
+    return this.props.questionOptionCount;
+  }
+
+  get numberOfRespondents() {
+    return this.poll.attributes.numberOfRespondents;
+  }
+
+  get numberOfResponses() {
+    return this.questionOptionCount.attributes.numberOfResponses;
+  }
+
+  get percent() {
+    return parseFloat(this.numberOfResponses) / this.numberOfRespondents * 100;
+  }
+
   render() {
-    return <div className="poll_results_chart_item"></div>;
+    if (this.questionOption === undefined) { return false; }
+
+    return (
+      <div className="poll_results_chart_item">
+        <span className="poll_results_chart_item__option_text">
+          {this.questionOption.attributes.text}
+        </span>
+        <Progress
+          percent={this.percent}
+          format={percent => `${percent}%`}
+          status="normal" />
+      </div>
+    );
   }
 }
 
--- frontend/src/components/PollResultsChartItem.test.js
+++ frontend/src/components/PollResultsChartItem.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Progress } from 'antd';
 import { shallow } from 'enzyme';
+import factories from '../../__factories__';
 import AbstractTestWrapper from '../utils/AbstractTestWrapper';
 import PollResultsChartItem from './PollResultsChartItem';
 
 class TestWrapper extends AbstractTestWrapper {
+  get optionText() {
+    return this.wrapper.find('.poll_results_chart_item__option_text').text();
+  }
+
+  get progress() {
+    return this.wrapper.find(Progress).first();
+  }
+
+  get givenPercent() {
+    return this.progress.prop('percent');
+  }
+
   _render() {
     return shallow(
       <PollResultsChartItem {...this.props} />
@@ -12,13 +26,42 @@ class TestWrapper extends AbstractTestWrapper {
 }
 
 describe('PollResultsChartItem', () => {
-  let component;
+  let poll, questionOptionCount, component;
 
   beforeEach(() => {
-    component = new TestWrapper();
+    poll = factories.poll.entity({ attributes: { numberOfRespondents: 100 }});
+    questionOptionCount = factories.questionOptionCount.entity({
+      attributes: { numberOfResponses: 10 },
+      relationships: {
+        poll: { id: poll.id, type: poll.type },
+        questionOption: factories.questionOption.identifier({ id: 400 }),
+      },
+    });
+    component = new TestWrapper({ props: { poll, questionOptionCount }});
   });
 
-  it('renders successful', () => {
-    expect(component.wrapper).toHaveLength(1);
+  describe('given questionOption', () => {
+    beforeEach(() => {
+      const questionOption = factories.questionOption.entity({
+        id: 400,
+        attributes: { text: '42' },
+        relationships: { question: poll.relationships.question },
+      });
+      component.props.questionOption = questionOption;
+    });
+
+    it('renders option text', () => {
+      expect(component.optionText).toEqual('42');
+    });
+
+    it('sets percentage of option', () => {
+      expect(component.givenPercent).toEqual(10);
+    });
+  });
+
+  describe('without questionOption', () => {
+    it('renders nothing', () => {
+      expect(component.wrapper.isEmptyRender()).toBe(true);
+    })
   });
 });
