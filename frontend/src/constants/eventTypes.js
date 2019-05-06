103ca7e22aa2c263c23507f88157b793864710fe
Patro <code@patrickrompf.de>
2019-05-06 14:25:45 +0200

    Add RESPONSE_CREATED event type

--- frontend/src/constants/eventTypes.js
+++ frontend/src/constants/eventTypes.js
@@ -1,3 +1,4 @@
 export const POLL_CREATED = 'POLL_CREATED';
 export const POLL_DESTROYED = 'POLL_DESTROYED';
 export const POLL_UPDATED = 'POLL_UPDATED';
+export const RESPONSE_CREATED = 'RESPONSE_CREATED';
