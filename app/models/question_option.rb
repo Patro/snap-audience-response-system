3c14f4ee8a45817d5cdee07d7f9a0d3e9ea36711
Patro <code@patrickrompf.de>
2019-05-08 22:32:16 +0200

    Order question options by position by default

--- app/models/question_option.rb
+++ app/models/question_option.rb
@@ -6,6 +6,8 @@ class QuestionOption < ApplicationRecord
            foreign_key: :picked_question_option_id,
            inverse_of: :picked_question_option, dependent: :destroy
 
+  default_scope { order(:position) }
+
   validates :text, presence: true
   validates :correct, inclusion: { in: [true, false] }
   validates :position, numericality: { greater_than_or_equal_to: 0 }
