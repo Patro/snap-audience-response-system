9db134c121a25828978f0cb33a4f0e3d56a63c5d
Patro <code@patrickrompf.de>
2019-07-07 13:50:41 +0200

    Add name to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_191328) do
+ActiveRecord::Schema.define(version: 2019_07_07_114747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2019_05_08_191328) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.string "name"
   end
 
   add_foreign_key "attendances", "interactive_sessions"
